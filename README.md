39eb4f6fe7303fec5f6f13cdb3fe0af9883ea348
emangelic <ema.angelic@outlook.com>
2024-08-10 13:19:50 -0400

    Testing push

---
+++ README.md
@@ -0,0 +1,1 @@
+I am makig a change
--- scripts.js
+++
@@ -1,8 +0,0 @@
-const emailInput = document.querySelector('#email');
-const passwordInput = document.querySelector('#password');
-const signUpButton = document.querySelector('#sign-up');
-const msgDiv = document.querySelector('#msg');
-const userEmailSpan = document.querySelector('#user-email');
-const userPasswordSpan = document.querySelector('#user-password');
-
-renderLastRegistered();
